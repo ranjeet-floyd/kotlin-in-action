6ca1b6b2147ef5b2f44e9ed10f10ccb0659b2474
Kumar, Ranjeet <ranjeet.kumar@rakuten.com>
2018-03-15 22:37:09 +0530

    added max fun

--- src/com/practice/HelloKotlin.kt
+++ src/com/practice/HelloKotlin.kt
@@ -3,4 +3,9 @@ package com.practice
 fun main(args:Array<String>){
 
     print("Hello Kotlin !!")
+    println("Max 5,4 is :" + max(5,4))
+}
+
+fun max (x: Int, y: Int): Int {
+    return if(x > y) x else y
 }
